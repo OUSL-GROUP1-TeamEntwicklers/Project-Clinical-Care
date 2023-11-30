fb7260f98eb3bc8547c4310efbce99c7b38e56c7
Kavindu Maleesha <91423119+Kavimaleesha@users.noreply.github.com>
2023-11-30 13:21:27 +0530

    Delete admin directory

--- admin/link_css.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css">  
